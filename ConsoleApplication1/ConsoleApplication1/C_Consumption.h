344d5dfd6a3118ea5465b78af8cc20bae86c240d
innit commit <student@12-10>
2021-07-20 20:29:38 +0300

    add: quantity_fuel

---
+++ ConsoleApplication1/ConsoleApplication1/C_Consumption.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "C_Calculate.h"
+namespace stv
+{
+    namespace consumption
+    {
+        class C_Count : public C_Calculate
+        {
+        public:
+            C_Count() : distance(0.0), quantity_fuel(0.0) {};
+            C_Count(float distance, float quantity_fuel) : distance(distance), quantity_fuel(quantity_fuel) {};
+            virtual float calculate()
+            {
+                return (quantity_fuel / distance) * 100;
+            }
+
+        private:
+            float distance;
+            float quantity_fuel;
+        };
+    }
+}
