cb3a550f0a3af0139d6f940757e125218b10391b
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-07-20 20:14:53 +0300

    add: release count

--- ConsoleApplication1/ConsoleApplication1/C_Count.h
+++ ConsoleApplication1/ConsoleApplication1/C_Count.h
@@ -1,10 +1,23 @@
 #pragma once
 #include "C_Calculate.h"
-namespace stv {
-    class C_Count :
-        public C_Calculate
+namespace stv
+{
+    namespace cost
     {
-    };
+        class C_Count : public C_Calculate
+        {
+        public:
+            C_Count() : distance(0.0), fuel_consumption(0.0), fuel_cost(0.0) {};
+            C_Count(float distance, float fuel_consumption, float fuel_cost) : distance(distance), fuel_consumption(fuel_consumption), fuel_cost(fuel_cost) {};
+            virtual float calculate()
+            {
+                return (distance * fuel_consumption * fuel_cost) / 100;
+            }
 
+        private:
+            float distance;
+            float fuel_consumption;
+            float fuel_cost;
+        };
+    }
 }
-
